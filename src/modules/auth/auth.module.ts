1e777415cfcc009bc81da36e923a77a1527c1fcb
Mohammed Salem <m7.salim3@gmail.com>
2026-01-11 18:32:25 +0200

    feat(auth):update auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { EnvVariables } from 'src/common/types/declaration-mergin';
 
 @Module({
+  imports:[
+    JwtModule.registerAsync({
+      global:true,
+      inject:[ConfigService],
+      useFactory:(configService:ConfigService<EnvVariables>) => ({
+        secret:configService.getOrThrow('JWT_SECRET'),
+        signOptions:{expiresIn:configService.getOrThrow('JWT_EXPIRES_IN')}
+      })
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
